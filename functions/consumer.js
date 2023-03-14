286d46f14f6b46c05a32343e40040ab2947866f1
Manoj Fernando <manojf.uom@gmail.com>
2023-03-14 11:02:41 +0530

    Lambda consumer update

--- functions/consumer.js
+++ functions/consumer.js
@@ -4,16 +4,17 @@ module.exports.handler = async (event) => {
   const records = event.Records;
   const FAILED_MESSAGE_NUM = 5;
 
-  for (const [i, record] of records) {
+  records.forEach((record, i) => {
     const data = record.kinesis.data;
 
     if (i === FAILED_MESSAGE_NUM) {
-      console.log("Error! ", sequenceNumber);
+      console.log("Error! ", record.kinesis.sequenceNumber);
       throw new Error("kaboom");
     }
     const decodedData = new Buffer.from(data, "base64").toString("utf-8");
     console.log(decodedData);
-  }
+  });
+
   return {
     statusCode: 200,
     body: "Success",
